c5c6baa9cce1ef6daa25d12e6c9f5d82d3bc5f2b
Mali <86328565+MaliMiau@users.noreply.github.com>
2025-02-14 18:46:11 -0800

    Clickable grid

---
+++ src/components/Category.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import Clickable from './Clickable.vue';
+const packs = [
+    "1",
+    "2",
+    "Rose Quartzzzzz",
+    "4",
+    "1",
+    "2",
+    "Rose Quartzzzzz",
+    "4",
+    "1",
+    "2",
+    "Rose Quartzzzzz",
+    "4",
+]
+</script>
+
+<template>
+    <div class="container">
+        <div class="grid">
+            <Clickable v-for="pack in packs":key="pack" :Name="pack"/>
+        </div>
+    </div>
+    
+</template>
+
+<style scoped>
+.grid {
+    display: grid;
+    width: 170px;
+    grid-template-columns: repeat(6, 1fr);
+}
+</style>
---
+++ src/components/Clickable.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+defineProps({
+    Name:{
+        type: String,
+        default: "Pack"
+    }
+})
+</script>
+
+<template>
+<div>
+    <img>
+    <section>{{ Name }}</section>
+</div>
+</template>
+
+<style scoped>
+    div{
+        background: #000000aa;
+        width: 150px;
+        height: 175px;
+        margin: 10px;
+        border-radius: 24px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        &:hover{
+            background: #000000ee;
+            cursor: pointer;
+        }
+    }
+    img {
+        width: 80px;
+        height: 80px;
+        background-color: black;
+        margin-top: 25px;
+    }
+    section {
+        margin-top: 10px;
+        color: white;
+        font-family: 'Courier New', Courier, monospace;
+        font-size: larger;
+        text-align: center;
+    }
+</style>
